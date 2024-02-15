4432fd376c1eeb8be4febc5b2da9d5a837fca91a
sarakn97 <nicholson.s@northeastern.edu>
2024-02-15 16:00:19 -0600

    minor updates to RSeQC & README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RNA-SEQ
 RNA-SEQ Pipeline created 2022
+## Programs:
+- R files have code pertaining to Modeling the data, differential expression analysis, WGCNA, GSEA, etc.
+- STAR.sh runs STAR sequencing alignment 
+- RSeQC.sh runs RSeQC to remove reads aligning to ribosomal genes after bam files have been indexed (samtools) 
--- RSeQC.sh
+++ RSeQC.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# Name : Sara Nicholson
+# Date : February 2024
+# Description: Run RSeQC after aligning reads to Genome in order to remove ribosomal genes. Reads not aligned to Ribosomal genes will be filtered into *.ex.bam. Must have samtools & RSeQc installed. 
 
 for i in /leyao/Active/saran/RNA2/STAR/*sortedByCoord.out.bam; do name=$(basename ${i} .sortedByCoord.out.bam);
        	echo ${name};
+	# Index Bam file via samtools (installed in path)
+	samtools index /leyao/Active/saran/RNA2/STAR/${name}.sortedByCoord.out.bam
+	# Remove Ribosomal Gene Reads (desired output will be *.ex.bam)
 	python3 /home/sara/Downloads/RSeQC-5.0.1/scripts/split_bam.py -i /leyao/Active/saran/RNA2/STAR/${name}.sortedByCoord.out.bam -r /home/sara/Downloads/RSeQC-5.0.1/scripts/hg38_rRNA.bed -o /leyao/Active/saran/RNA2/finalRNAdata/RSeQC_Out/${name}_ ; done
 
